fe753b14467e7a4e3a06ead5e63db1e626b82b46
Peter Abeles <peter.abeles@gmail.com>
2020-04-26 16:33:55 -0700

    - build jar

---
+++ src/main/java/ShowWebcamInWindow.java
@@ -0,0 +1,24 @@
+import boofcv.gui.image.ImagePanel;
+import boofcv.gui.image.ShowImages;
+import boofcv.io.webcamcapture.UtilWebcamCapture;
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.ds.raspberrypi.RaspiYUVDriver;
+
+public class ShowWebcamInWindow {
+    // This is required for running on a Raspberry PI
+    static {
+        Webcam.setDriver(new RaspiYUVDriver());
+    }
+
+    public static void main(String[] args) {
+        Webcam webcam = UtilWebcamCapture.openDefault(640,480);
+        System.out.println("Opened camera "+webcam.getName());
+
+        ImagePanel gui = new ImagePanel();
+        gui.setPreferredSize(webcam.getViewSize());
+        ShowImages.showWindow(gui,"Webcam",true);
+        while( true ) {
+            gui.setImageRepaint(webcam.getImage());
+        }
+    }
+}
--- src/main/java/WebcamScanQR.java
+++ src/main/java/WebcamScanQR.java
@@ -10,9 +10,7 @@ import com.github.sarxos.webcam.ds.raspberrypi.RaspiYUVDriver;
 import java.awt.image.BufferedImage;
 import java.util.List;
 
-/**
- * @author Peter Abeles
- */
+
 public class WebcamScanQR {
 
     // This is required for running on a Raspberry PI
@@ -21,9 +19,13 @@ public class WebcamScanQR {
     }
 
     public static void main(String[] args) {
-        QrCodeDetector<GrayU8> detector = FactoryFiducial.qrcode(null,GrayU8.class);
-
+        // Open the first webcam it sees at attempt to open it at 640x480
         Webcam webcam = UtilWebcamCapture.openDefault(640,480);
+
+        System.out.println("Opened camera "+webcam.getName());
+
+        // Create the detector
+        QrCodeDetector<GrayU8> detector = FactoryFiducial.qrcode(null,GrayU8.class);
 
         int frame = 0;
         GrayU8 gray=null;
@@ -32,15 +34,22 @@ public class WebcamScanQR {
             BufferedImage image = webcam.getImage();
             gray = ConvertBufferedImage.convertFrom(image,gray);
 
+            long time0 = System.nanoTime();
             detector.process(gray);
+            long time1 = System.nanoTime();
+            double milli = (time1-time0)*1e-6;
+
+            // Print out results and some diagnostic info
+            System.out.printf("%04d processed %d x %d in %4.2f (ms)\n",frame,gray.width,gray.height,milli);
+
             List<QrCode> detected = detector.getDetections();
             if( detected.isEmpty() ) {
-                System.out.println(frame+" Nothing detected");
+                System.out.println("  Nothing detected");
                 continue;
             }
-            System.out.println(frame+" Detected Total "+detected.size());
+            System.out.println("  Found "+detected.size());
             for( QrCode qr : detected ) {
-                System.out.println("Found: "+qr.message);
+                System.out.println("     QR \""+qr.message+"\"");
             }
         }
     }
